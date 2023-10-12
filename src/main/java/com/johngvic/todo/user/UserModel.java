55e2ac94d7cc485b93be33b4385d838e5df622a7
joao <duartejoaovictor@johndeere.com>
2023-10-12 14:22:29 -0300

    initial commit

---
+++ src/main/java/com/johngvic/todo/user/UserModel.java
@@ -0,0 +1,11 @@
+package com.johngvic.todo.user;
+
+public class UserModel {
+  private String username;
+  private String name;
+  private String password;
+
+  public String getName() {
+    return this.name;
+  }
+}
